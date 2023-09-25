aa9f8dc32bc3552ab039f9ca3a862a36d211f4ca
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-09-25 23:46:21 +0200

    add parallex background basic

--- srcs/core/background.c
+++ srcs/core/background.c
@@ -6,7 +6,7 @@
 /*   By: sebasnadu <johnavar@student.42berlin.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 12:36:49 by sebasnadu         #+#    #+#             */
-/*   Updated: 2023/09/25 20:23:25 by sebasnadu        ###   ########.fr       */
+/*   Updated: 2023/09/25 23:44:47 by sebasnadu        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	draw_block_bg(int x, int y, void *sprite, t_game *game)
 {
+	if (x > WIN_WIDTH || y > WIN_HEIGHT)
+		return ;
 	mlx_put_image_to_window(game->mlx, game->win, sprite,
 		x + game->g_offset.x, y + game->g_offset.y);
 }
@@ -21,11 +23,18 @@ void	draw_block_bg(int x, int y, void *sprite, t_game *game)
 void	background(t_game *game)
 {
 	int	i;
+	int	j;
+	int	x_offset;
+	int	y_offset;
 
 	i = -1;
+	j = 1;
+	y_offset = BPX + game->p_offset.y;
 	while (++i < bg_n)
 	{
-		draw_block_bg((3 * BPX) - (BPX + game->player->pos.x), BPX + game->p_offset.y, game->sprites.bg[i], game);
-		draw_block_bg((16 * BPX) - (BPX + game->player->pos.x), BPX + game->p_offset.y, game->sprites.bg[i], game);
+		x_offset = (((j * BPX) - (game->player->pos.x)) / (j * 4));
+		draw_block_bg(x_offset - BPX, y_offset, game->sprites.bg[i], game);
+		draw_block_bg((8 * BPX) + x_offset, y_offset, game->sprites.bg[i], game);
+		j++;
 	}
 }
